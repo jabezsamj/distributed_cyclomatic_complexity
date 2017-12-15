31fbf8e07d1b8e4f981d36afb643b2fe18736c1b
johnsamjohn <jabezsamj@gmail.com>
2017-12-15 19:11:33 +0000

    Deleting folders that are not relevant

--- Cyclomatic_slave_1/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import System.Environment (getArgs)
-import Argon (parseCode)
-
-main :: IO ()
-main = do
-    (path:_) <- getArgs
-    print . parseCode (Just path) =<< readFile path
